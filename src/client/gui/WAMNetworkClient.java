aee86673e27e6565373aa6e86e5c8a34ad9d95dc
Sam Chilaka <soc5881@rit.edu>
2019-04-12 02:24:08 -0400

    update - sam

---
+++ src/client/gui/WAMNetworkClient.java
@@ -0,0 +1,80 @@
+package client.gui;
+
+import common.WAMProtocol;
+
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+import static common.WAMProtocol.*;
+
+public class WAMNetworkClient {
+
+    /** Turn on if standard output debug messages are desired. */
+    private static final boolean DEBUG = false;
+
+    /**
+     * Print method that does something only if DEBUG is true
+     *
+     * @param logMsg the message to log
+     */
+    private static void dPrint( Object logMsg ) {
+        if ( WAMNetworkClient.DEBUG ) {
+            System.out.println( logMsg );
+        }
+    }
+
+    /** client socket to communicate with server */
+    private Socket client;
+    /** used to read requests from the server */
+    private Scanner networkIn;
+    /** Used to write responses to the server. */
+    private PrintStream networkOut;
+    /** the model which keeps track of the game */
+    private WAM wam;
+    /** sentinel loop used to control the main loop */
+    private boolean go;
+
+    /**
+     * Accessor that takes multithreaded access into account
+     *
+     * @return whether it ok to continue or not
+     */
+    private synchronized boolean goodToGo() {
+        return this.go;
+    }
+
+    /**
+     * Multithread-safe mutator
+     */
+    private synchronized void stop() {
+        this.go = false;
+    }
+
+    /**
+     * Called when the server sends a message saying that
+     * gameplay is damaged. Ends the game.
+     *
+     * @param arguments The error message sent from the reversi.server.
+     */
+    public void error( String arguments ) {
+        WAMNetworkClient.dPrint( '!' + ERROR + ',' + arguments );
+        dPrint( "Fatal error: " + arguments );
+        wam.error( arguments );
+        this.stop();
+    }
+
+    public WAMNetworkClient(String host, int port, WAM wam) {
+        try {
+            client = new Socket(host, port);
+            networkIn = new Scanner(client.getInputStream());
+            networkOut = new PrintStream(client.getOutputStream());
+            wam = wam;
+            go = true;
+
+            String request = networkIn.next();
+            String args = networkIn.nextLine();
+            if (!request.equals(WAMProtocol.))
+        }
+    }
+}
---
+++ src/common/WAMGUI.java
@@ -0,0 +1,18 @@
+package common;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class WAMGUI {
+    public static void main(String[] args) {
+
+    }
+}
