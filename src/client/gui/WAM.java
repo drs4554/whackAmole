b3efb343274489bf9ff6e75b5c38e93d7c1b0e85
drs4554 <drs4554@rit.edu>
2019-04-13 16:20:18 -0400

    model trial

--- src/client/gui/WAM.java
+++ src/client/gui/WAM.java
@@ -1,5 +1,75 @@
 package client.gui;
 
+import java.util.List;
+
 public class WAM {
 
+    //model
+
+    private int ROWS;
+
+    private int COLS;
+
+    private int points;
+
+    /** the observers of this model */
+    private List<Observer<WAM>> observers;
+
+    public enum status {
+        UP,
+        DOWN;
+    }
+
+    public status[] holes;
+
+    /**
+     * The view calls this method to add themselves as an observer of the model.
+     *
+     * @param observer the observer
+     */
+    public void addObserver(Observer<WAM> observer) {
+        this.observers.add(observer);
+    }
+
+    /** when the model changes, the observers are notified via their update() method */
+    private void alertObservers() {
+        for (Observer<WAM> obs: this.observers ) {
+            obs.update(this);
+        }
+    }
+
+    public WAM(int rows, int cols) {
+        this.ROWS = rows;
+        this.COLS = cols;
+        this.points = 0;
+
+        this.holes = new status[COLS * ROWS];
+        for (int i = 0; i < COLS * ROWS; i++) {
+            this.holes[i] = status.DOWN;
+        }
+    }
+
+    public void moleUP(int num) {
+        holes[num] = status.UP;
+    }
+
+    public void moleDOWN(int num) {
+        holes[num] = status.DOWN;
+    }
+
+    public void gameWon() {
+        alertObservers();
+    }
+
+    public void gameLost() {
+        alertObservers();
+    }
+
+    public void gameTied() {
+        alertObservers();
+    }
+
+    public void updateScore(int score) {
+        this.points = score;
+    }
 }
