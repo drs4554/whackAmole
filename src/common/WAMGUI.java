9855b345604325f9566a57e9fa7e14dfe5cf564d
Sam Chilaka <soc5881@rit.edu>
2019-04-13 16:51:36 -0400

    update - sam

--- src/common/WAMGUI.java
+++ src/common/WAMGUI.java
@@ -1,5 +1,7 @@
 package common;
 
+import client.gui.WAM;
+import client.gui.WAMNetworkClient;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Pos;
@@ -11,8 +13,22 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-
+/**
+ * JavaFX GUI for a connected game of Whack-A-Mole
+ *
+ * @author Sam Chilaka @<soc5881@rit.edu>
+ * @author Dhaval Shrimshal
+ */
 public class WAMGUI {
+
+    private WAM wam;
+
+    private Button[] holes;
+
+    private WAMNetworkClient client;
+
+    Label labels = new Label();
+
     public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("Usage: java WhackAmole host port");
