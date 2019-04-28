faaa1a15241e68f9954f6b2509ee9eedd3b86dc3
Sam Chilaka <soc5881@rit.edu>
2019-04-28 17:38:33 -0400

    update - sam

--- src/server/WAMPlayer.java
+++ src/server/WAMPlayer.java
@@ -12,13 +12,16 @@ import java.util.Scanner;
  * @author Sam Chilaka
  * @author Dhaval Shrimshal
  */
-public class WAMPlayer implements WAMProtocol, Closeable {
+public class WAMPlayer extends Thread implements WAMProtocol, Closeable  {
 
     private Socket socket;
     private Scanner scanner;
     private PrintStream printer;
 
+
     /**
+     * Creates a new {@link WAMPlayer} that will be used to the {@link Socket} to
+     * communicate with the client/player.
      *
      * @param socket
      * @throws IOException
@@ -33,6 +36,60 @@ public class WAMPlayer implements WAMProtocol, Closeable {
         }
     }
 
-    public void connect() { printer.println(WELCOME);}
 
+    /**
+     * Sends the initial {@link //CONNECT} request to the client
+     */
+    public void connect() { printer.println(WELCOME); }
+
+
+    /**
+     * Called to send an {@link #ERROR} to the client. This is called if either
+     * client has invalidated themselves with a bad response.
+     *
+     * @param message error message
+     */
+    public void error(String message) {
+        printer.println(ERROR + " " + message);
+    }
+
+
+    /**
+     * Called to send a {@link #GAME_WON} message to the client because the player's
+     * most recent move won the game
+     */
+    public void gameWon() {
+        printer.println(GAME_WON);
+    }
+
+
+    /**
+     * Called to send a {@link #GAME_LOST} message to the client because the player's
+     * most recent move won the game
+     */
+    public void gameLost() {
+        printer.println(GAME_LOST);
+    }
+
+
+    /**
+     * Called to send a {@link #GAME_TIED} message to the client because the player's
+     * most recent move won the game
+     */
+    public void gameTied() {
+        printer.println(GAME_TIED);
+    }
+
+
+    /**
+     * Called to close the clients when the game is over
+     */
+    @Override
+    public void close() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+             System.err.println(e);
+        }
+    }
 }
